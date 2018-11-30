1a74fa2dad166df5cef78165a0b8b17121cbab13
kzyo <kzy.onzw@gmail.com>
2018-11-30 22:11:08 +0900

    made task MVC

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times {Task.create!(name: 'sample task')}
+2.times {Task.create!(name: 'done task', is_done: true)}
